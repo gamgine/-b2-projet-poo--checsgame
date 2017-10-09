0805184cb5c6e032b8c3d1e952b9a37b0966a68e
gamgine <victor.fromentin95@ynov.com>
2017-10-10 00:30:27 +0200

    Add files via upload

--- [b2]projetPOO-checsgame/Entity.cs
+++ [b2]projetPOO-checsgame/Entity.cs
@@ -7,6 +7,7 @@ namespace _b2_projetPOO_checsgame
 {
     class Entity
     {
+        private int[,] mv;
         protected bool color;
         public Entity(bool color)
         { this.color = color; }
@@ -15,13 +16,25 @@ namespace _b2_projetPOO_checsgame
         public bool GetColor()
         { return this.color; }
 
-        public virtual int[,] Mv(int[]s)
-        {   return new int [0,0];   }
-        public virtual int[,] Att(int[]s)
+        public int[,] Mv(int[]s)
         {
-            int[,] mv = new int[0, 0];
-            return mv;
+            int ml = 0;
+            int[,] r = { };
+            for (int i = 0; i < this.mv.Length; i++)
+            {
+                int p = this.mv[i, 0] + s[0];
+                int p2 = this.mv[i, 1] + s[1];
+                if ((p < 8 && p > 0) && (p2 < 8 && p2 > 0))
+                {
+                    r[ml, 0] = p;
+                    r[ml, 1] = p2;
+                    ml++;
+                }
+            }
+            return r;
         }
+        public int[,] Att(int[]s)
+        { return this.Mv(s); }
         public virtual bool isk()
         { return false; }
     }//todo definir tous elements et verif bugs
