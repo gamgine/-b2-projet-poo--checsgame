5effbd34a65c8544a576ee9dd4f8937165782cef
gamgine <victor.fromentin95@ynov.com>
2017-10-10 00:51:15 +0200

    Add files via upload

--- [b2]projetPOO-checsgame/entity/Pion.cs
+++ [b2]projetPOO-checsgame/entity/Pion.cs
@@ -8,20 +8,24 @@ namespace _b2_projetPOO_checsgame.entity
 {
     class Pion : Entity
     {
-        private int[,] mv = new int[3, 2];
+        private int[,] mv = new int[1, 2];
         public Pion(bool color) : base(color)
         {
             if (this.color) { mv[0, 0] = 1; }
             else { mv[0, 0] = -1; }
             mv[0, 1] = 0;
+        }
+        public override int[,] Att(int[] s)
+        {
+            int[,] att = new int[2, 2];
+            if (this.color) { att[1, 0] = (1 + s[0]); }
+            else { att[1, 0] = (-1 + s[0]); }
+            att[1, 1] = (1 + s[1]);
 
-            if (this.color) { mv[1, 0] = 1; }
-            else { mv[1, 0] = -1; }
-            mv[1, 1] = 1;
-
-            if (this.color) { mv[2, 0] = 1; }
-            else { mv[2, 0] = -1; }
-            mv[2, 1] = -1;
+            if (this.color) { att[0, 0] = (1 + s[0]); }
+            else { att[0, 0] = (-1 + s[0]); }
+            att[0, 1] = (-1 + s[1]);
+            return att;
         }
         public override string Get()
         { return " p"; }
